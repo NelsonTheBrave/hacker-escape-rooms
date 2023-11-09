cc6322906e346bb71b53b5bdbb2417f0a1acb1be
Nelson <sherman.erik@gmail.com>
2023-11-09 15:27:00 +0100

    added mobile menu with animation

---
+++ main.js
@@ -0,0 +1,27 @@
+// Variables
+const navBar = document.querySelector(".navBar");
+
+// Event listeners
+document.querySelector(".navbar-button").addEventListener("click", openPopup);
+visualViewport.onresize = closePopup;
+
+//Event handlers
+function openPopup() {
+  const html = document.querySelector("html");
+  html.setAttribute("class", "--transparant");
+  setTimeout(() => {
+    html.classList.remove("--transparant");
+    navBar.setAttribute("class", "navBar--popup");
+  }, 200);
+
+  navBar.addEventListener("click", closePopup);
+}
+function closePopup(event) {
+  if (
+    event.target.nodeName === "BUTTON" ||
+    event.target.nodeName == "A" ||
+    event.target.width > 1200
+  ) {
+    navBar.setAttribute("class", "navBar");
+  }
+}
